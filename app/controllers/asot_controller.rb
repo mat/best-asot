8ce5fad43d145892b6480d3d0ba5b446eb4042b2
Matthias Lüdtke <github@matthias-luedtke.de>
2008-10-31 22:34:00 +0100

    DRY up the view.

--- app/controllers/asot_controller.rb
+++ app/controllers/asot_controller.rb
@@ -1,8 +1,8 @@
 class AsotController < ApplicationController
   def index
-    @top5 = Asot.all(:order => 'votes DESC', :limit => 10)
-    @last5 = Asot.all(:order => 'no DESC', :limit => 3)
-    @asots = Asot.all(:order => 'no DESC')
+    @top        = Asot.all(:order => 'votes DESC', :limit => 10)
+    @latest     = Asot.all(:order => 'no DESC', :limit => 3)
+    @all_asots  = Asot.all(:order => 'no DESC')
   end
 
 end
