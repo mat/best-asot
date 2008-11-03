3cd86f62be3f8bd83af515b0ecf2c6d44a9dab81
Matthias Lüdtke <github@matthias-luedtke.de>
2008-11-03 22:48:13 +0100

    Major view changes.

--- app/controllers/asot_controller.rb
+++ app/controllers/asot_controller.rb
@@ -2,7 +2,15 @@ class AsotController < ApplicationController
   def index
     @by_votes   = Asot.all(:order => 'votes DESC')
     @latest     = Asot.all(:order => 'no DESC', :limit => 3)
-    @all_asots  = Asot.all(:order => 'no DESC')
+
+    @top_2008   = Asot.all(:order => 'votes DESC').find_all{|a| a.airdate && a.airdate.year == 2008}
+    @episodes_2008 = Asot.all(:order => 'no DESC').find_all{|a| a.airdate && a.airdate.year == 2008}
+
+    @top_2007   = Asot.all(:order => 'votes DESC').find_all{|a| a.airdate && a.airdate.year == 2007}
+    @episodes_2007 = Asot.all(:order => 'no DESC').find_all{|a| a.airdate && a.airdate.year == 2007}
+
+    @top_2006   = Asot.all(:order => 'votes DESC').find_all{|a| a.airdate && a.airdate.year == 2006}
+    @episodes_2006 = Asot.all(:order => 'no DESC').find_all{|a| a.airdate && a.airdate.year == 2006}
   end
 
 end
