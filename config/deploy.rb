dd9bfa3c45321fa4ab93dc8032fc1bef196364ec
Matthias Lüdtke <github@matthias-luedtke.de>
2008-11-09 20:00:55 +0100

    cap deploy:check works, at least.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,15 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "asot"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/mat/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :repository, "git://github.com/mat/best-asot.git"
+set :branch, "master"
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+server "better-idea.org", :app, :web, :db, :primary => true
+
