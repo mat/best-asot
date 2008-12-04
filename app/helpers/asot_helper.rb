bc5787acd23bc0153c66cd1240a02f1d4c12f5fb
Matthias Lüdtke <github@matthias-luedtke.de>
2008-12-04 20:02:54 +0100

    Red ON AIR marker for live episodes.

--- app/helpers/asot_helper.rb
+++ app/helpers/asot_helper.rb
@@ -6,4 +6,14 @@ module AsotHelper
   def by_airdate?
     @order.index('airdate')
   end
+
+  def format_airdate(asot)
+    unless asot.airdate.today?
+      return "<td>#{asot.airdate.strftime("%d-%b-%Y")}</td>"
+    end
+
+    if (20..21).include? Time.now.hour 
+      return "<td class='onair'><a href='#{asot.url}'>On air!</a></td>"
+    end
+  end
 end
