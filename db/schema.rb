568b4930eedf357cb30c95d9efcdd8f328f670ee
Matthias Lüdtke <github@matthias-luedtke.de>
2008-12-05 12:30:39 +0100

    Let votes default to 0. Think of fresh ASOT with no vote cast yet.

--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081030123823) do
+ActiveRecord::Schema.define(:version => 20081205112308) do
 
   create_table "asots", :force => true do |t|
     t.integer  "no"
     t.text     "url"
-    t.integer  "votes"
+    t.integer  "votes",      :default => 0, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "airdate"
