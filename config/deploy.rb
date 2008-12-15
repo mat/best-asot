eb5bf753debefe45923a0b9c4bb076a6cd3a80ea
Matthias Lüdtke <github@matthias-luedtke.de>
2008-12-15 12:31:40 +0100

    Capistrano changes.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,11 @@
-set :application, "asot"
+set :application, "best-asot"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/home/mat/www/#{application}"
+
+set :use_sudo, false
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
