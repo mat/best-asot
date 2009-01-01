505a2dfb494f04d2548ba1105c2976607d8cfcc7
Matthias Lüdtke <github@matthias-luedtke.de>
2009-01-01 19:07:19 +0100

    Added 2009 section.

--- app/controllers/asot_controller.rb
+++ app/controllers/asot_controller.rb
@@ -5,6 +5,7 @@ class AsotController < ApplicationController
     @by_votes      = Asot.all(:order => 'votes DESC', :limit => 10)
     @order         = params[:order]
 
+    @episodes_2009 = Asot.find_by_year(2009, @order)
     @episodes_2008 = Asot.find_by_year(2008, @order)
     @episodes_2007 = Asot.find_by_year(2007, @order)
     @episodes_2006 = Asot.find_by_year(2006, @order)
