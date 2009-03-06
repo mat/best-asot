0109d247b22c98bc9ce70493fcc4ffadc3a97a80
Matthias Lüdtke <github@matthias-luedtke.de>
2009-03-06 21:10:18 +0100

    Heavy rebuilding going on. Keep out. ;)

--- lib/asot.rb
+++ lib/asot.rb
@@ -1,9 +1,47 @@
-class AsotController < ApplicationController
-  caches_page :index
+#!/usr/bin/env ruby
 
-  def index
+require 'rubygems'
+require 'sinatra'
+require 'activerecord'
+
+require 'lib/models'
+
+#ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :dbfile =>  'db/development.sqlite3'
+)
+
+set :views,  File.expand_path(File.join(File.dirname(__FILE__),'..','views'))
+set :public,  File.expand_path(File.join(File.dirname(__FILE__),'..','public'))
+
+#Sinatra::Application.default_options.merge!(
+  #:views => File.join(ROOT_DIR, '..', 'views')
+  #:app_file => File.join(ROOT_DIR, 'drewolson.rb'),
+  #:run => false
+#) 
+
+
+
+      def partial(template, *args)
+        options = args.extract_options!
+        options.merge!(:layout => false)
+        path = template.to_s.split(File::SEPARATOR)
+        object = path[-1].to_sym
+        path[-1] = "_#{path[-1]}"
+        template = File.join(path).to_sym
+        if collection = options.delete(:collection)
+          collection.inject([]) do |buffer, member|
+              buffer << erb(template, options.merge(:layout => false, :locals => {object => member}))
+          end.join("\n")
+        else
+          erb(template, options)
+        end
+end
+
+get '/' do
     @by_votes      = Asot.all(:order => 'votes DESC', :limit => 10)
-    @order         = params[:order]
+    @order         = 'airdate DESC'
 
     @episodes_2009 = Asot.find_by_year(2009, @order)
     @episodes_2008 = Asot.find_by_year(2008, @order)
@@ -11,5 +49,7 @@ class AsotController < ApplicationController
     @episodes_2006 = Asot.find_by_year(2006, @order)
 
     @last_update   = Asot.last_update
-  end
+
+    erb :index
 end
+
