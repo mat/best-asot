df7fcc400ae8f250e0951e9035c60e93c67cd7fb
Matthias Lüdtke <github@matthias-luedtke.de>
2009-03-07 09:19:12 +0100

    New output matches old.

--- lib/asot.rb
+++ lib/asot.rb
@@ -5,6 +5,7 @@ require 'sinatra'
 require 'activerecord'
 
 require 'lib/models'
+require 'lib/helpers'
 
 #ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.establish_connection(
@@ -21,24 +22,6 @@ set :public,  File.expand_path(File.join(File.dirname(__FILE__),'..','public'))
   #:run => false
 #) 
 
-
-
-      def partial(template, *args)
-        options = args.extract_options!
-        options.merge!(:layout => false)
-        path = template.to_s.split(File::SEPARATOR)
-        object = path[-1].to_sym
-        path[-1] = "_#{path[-1]}"
-        template = File.join(path).to_sym
-        if collection = options.delete(:collection)
-          collection.inject([]) do |buffer, member|
-              buffer << erb(template, options.merge(:layout => false, :locals => {object => member}))
-          end.join("\n")
-        else
-          erb(template, options)
-        end
-end
-
 get '/' do
     @by_votes      = Asot.all(:order => 'votes DESC', :limit => 10)
     @order         = 'airdate DESC'
@@ -53,3 +36,16 @@ get '/' do
     erb :index
 end
 
+get '/by-rank' do
+    @by_votes      = Asot.all(:order => 'votes DESC', :limit => 10)
+    @order         = 'votes DESC'
+
+    @episodes_2009 = Asot.find_by_year(2009, @order)
+    @episodes_2008 = Asot.find_by_year(2008, @order)
+    @episodes_2007 = Asot.find_by_year(2007, @order)
+    @episodes_2006 = Asot.find_by_year(2006, @order)
+
+    @last_update   = Asot.last_update
+
+    erb :index
+end
---
+++ lib/helpers.rb
@@ -0,0 +1,39 @@
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+
+  def by_votes? ; @order.index('votes') ; end
+  def by_airdate? ; @order.index('airdate') ; end
+
+  def format_airdate(asot)
+    unless asot.airdate.today?
+      return "<td>#{asot.airdate.strftime("%d-%b-%Y")}</td>"
+    end
+
+    if (10..19).include? Time.now.hour 
+     asot.url = h 'http://www.di.fm/calendar/calendar.php?type=day&calendar=2'
+     return "<td class='onair'><a href='#{asot.url}' target='_blank'>Coming soon</a></td>"
+    elsif (20..21).include? Time.now.hour 
+     return "<td class='onair'><a href='#{asot.url}' target='_blank'>On air!</a></td>"
+    else
+     return "<td>#{asot.airdate.strftime("%d-%b-%Y")}</td>"
+    end
+  end
+
+  def partial(template, *args)
+    options = args.extract_options!
+    options.merge!(:layout => false)
+    path = template.to_s.split(File::SEPARATOR)
+    object = path[-1].to_sym
+    path[-1] = "_#{path[-1]}"
+    template = File.join(path).to_sym
+    if collection = options.delete(:collection)
+      collection.inject([]) do |buffer, member|
+        buffer << erb(template, options.merge(:layout => false, :locals => {object => member}))
+      end.join("\n")
+    else
+      erb(template, options)
+    end
+  end
+end
+
