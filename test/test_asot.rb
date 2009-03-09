28796a10d590afe74956284d9e499a41548a7822
Matthias Lüdtke <github@matthias-luedtke.de>
2009-03-09 09:38:41 +0100

    Ported missing tests.

--- test/test_asot.rb
+++ test/test_asot.rb
@@ -5,7 +5,15 @@ require 'activerecord'
 require 'lib/models'
 require 'test/unit'
 
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :dbfile =>  'db/test.sqlite3'
+)
+
 class AsotTest < Test::Unit::TestCase
+  def setup
+    Asot.delete_all
+  end
 
   def test_grep_the_forum_url_given_an_episode_number
     uri = Asot.fetch_di_uri(369)
@@ -22,7 +30,7 @@ class AsotTest < Test::Unit::TestCase
     assert_equal Time.local(2008,9,11), airdate
   end
 
-  def TODO_test_calculate_rank_based_on_votes
+  def test_calculate_rank_based_on_votes
     a = Asot.create(:no => 1, :votes => 1)
     b = Asot.create(:no => 2, :votes => 10)
     c = Asot.create(:no => 3, :votes => 100)
@@ -33,13 +41,13 @@ class AsotTest < Test::Unit::TestCase
     assert_equal 1, c.rank
   end
 
-  def TODO_test_add_new_episode_with_votes_and_airdate
+  def test_add_new_episode_with_votes_and_airdate
     url = 'http://forums.di.fm/trance/armin-van-buuren-presents-state-of-trance-episode-377-a-150099/'
     a = Asot.add_by_url_and_fetch(url)
 
     assert_equal 377, a.no
     assert_equal 32, a.votes
-    assert_equal Time.parse('Thu, 6 Nov 2008'), a,airdate
+    assert_equal Time.parse('Thu, 6 Nov 2008'), a.airdate
   end
 
 end
