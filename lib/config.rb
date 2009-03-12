b86858bf3392617a0f0a1f59347b2bb73298199b
Matthias Lüdtke <github@matthias-luedtke.de>
2009-03-12 19:04:25 +0100

    configure :test, :development and :production individually.

--- lib/config.rb
+++ lib/config.rb
@@ -1,13 +1,28 @@
+
+configure :development do
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :dbfile =>  'db/development.sqlite3'
+  )
+end
+
+configure :production do
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :dbfile =>  'db/production.sqlite3'
+  )
+end
+
+configure :test do
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :dbfile =>  'db/test.sqlite3'
+  )
+end
+
 configure do
-
-#ActiveRecord::Base.logger = Logger.new(STDOUT)
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :dbfile =>  'db/development.sqlite3'
-)
-
-set :views,  File.expand_path(File.join(File.dirname(__FILE__),'..','views'))
-set :public,  File.expand_path(File.join(File.dirname(__FILE__),'..','public'))
-
+  set :views,   File.expand_path(File.join(File.dirname(__FILE__),'..','views'))
+  set :public,  File.expand_path(File.join(File.dirname(__FILE__),'..','public'))
 end
 
