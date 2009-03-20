276181228fc88bd59db96c9d1f8a9c3685de19c4
Matthias Lüdtke <github@matthias-luedtke.de>
2009-03-20 14:31:49 +0100

    Table cell colors match graph colors again.

--- lib/asot.rb
+++ lib/asot.rb
@@ -26,8 +26,12 @@ require 'lib/helpers'
     @order = 'votes DESC' if request.path_info == '/by-rank'
 
     @episodes = {}
+    @top5 = {}
     Asot::YEARS.each do |y|
       @episodes[y] = Asot.find_by_year(y, @order)
+
+      top5 = Asot.find_by_year(y, 'votes DESC')[0..4]
+      @top5[y] = top5.sort_by{|a| a.no} if top5.length == 5
     end
 
     erb :index
