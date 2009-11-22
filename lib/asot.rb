04fc3160a3153fbc6a35baf8e58b13744ce49e91
mat <github@matthias-luedtke.de>
2009-11-22 20:07:41 +0100

    /admin only can edit_in_place the notes.

--- lib/asot.rb
+++ lib/asot.rb
@@ -17,30 +17,46 @@ require 'lib/helpers'
 ["/", "/by-rank"].each do |path|
   get path do
     response['Cache-Control'] = 'public, max-age=120'
-    @last_update = Asot.last_update
-    last_modified @last_update
+    last_modified(Asot.last_update)
 
-    @top_ten = Asot.all(:order => 'votes DESC', :limit => 10)
-
-    @order = 'airdate DESC'
-    @order = 'votes DESC' if request.path_info == '/by-rank'
-
-    @episodes = {}
-    @top5 = {}
-    Asot::YEARS.each do |y|
-      @episodes[y] = Asot.find_by_year(y, @order)
-
-      top5 = Asot.find_by_year(y, 'votes DESC')[0..4]
-      @top5[y] = top5.sort_by{|a| a.no} if top5.length == 5
-    end
-
-    Asot.calc_ranks
+    @is_admin = false
+    do_it()
     erb :index
   end
+end
+
+get "/admin" do
+    use Rack::Auth::Basic do |username, password|
+      username == 'admin' && password == 'secret'
+    end
+
+    @is_admin = true
+    do_it()
+    erb :index
 end
 
 post "/update_note" do
   asot = Asot.find_by_no(params[:element_id].to_i)
   asot.notes = params[:update_value]
   asot.save!
+end
+
+
+def do_it
+  @last_update = Asot.last_update
+  @top_ten = Asot.all(:order => 'votes DESC', :limit => 10)
+
+  @order = 'airdate DESC'
+  @order = 'votes DESC' if request.path_info == '/by-rank'
+
+  @episodes = {}
+  @top5 = {}
+  Asot::YEARS.each do |y|
+    @episodes[y] = Asot.find_by_year(y, @order)
+
+    top5 = Asot.find_by_year(y, 'votes DESC')[0..4]
+    @top5[y] = top5.sort_by{|a| a.no} if top5.length == 5
+  end
+
+  Asot.calc_ranks
 end
